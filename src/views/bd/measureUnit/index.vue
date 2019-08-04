71a49994dbad3ac9da07f19ca7b91977571e6697
zhaojun <310499545@qq.com>
2019-08-04 10:04:24 +0800

    新增计量单位页面

---
+++ src/views/bd/measureUnit/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-container">
+    111
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+    data() {
+      return {
+
+      }
+    },
+    created() {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
