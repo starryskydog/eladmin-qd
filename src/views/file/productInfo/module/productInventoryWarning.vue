4d08086eeb3739e28c55bb3da68d17ab166533e6
starrysky <838252223@qq.com>
2019-08-20 23:28:53 +0800

    产品资料  仓库告警设置

--- src/views/file/productInfo/module/productInventoryWarning.vue
+++ src/views/file/productInfo/module/productInventoryWarning.vue
@@ -11,7 +11,14 @@
         <el-table-column v-for="(v,i) in product_inventory_warning.columns" :key="v.field" :prop="v.field" :label="v.title" :width="v.width">
           <template slot-scope="scope">
             <span >
-              <el-input v-if="!v.type" size="mini" placeholder="请输入内容" v-model="productInventoryWarningList[scope.$index][v.field]" @change="((val)=>{setProductInventoryWarning(val,scope.$index,v.field)})"/>
+              <el-input v-if="!v.type" v-model="productInventoryWarningList[scope.$index][v.field]" size="mini" placeholder="请输入内容" @change="((val)=>{setProductInventoryWarning(val,scope.$index,v.field)})"/>
+              <el-select v-else v-model="wareHouseList[scope.$index][v.field]" size="small" style="width: 150px;" @change="((val)=>{setProductInventoryWarning(val,scope.$index,v.field)})">
+                <el-option
+                  v-for="(item, index) in wareHouseList"
+                  :key="item.id"
+                  :label="item.name"
+                  :value="item.id"/>
+              </el-select>
             </span>
           </template>
         </el-table-column>
@@ -21,14 +28,17 @@
 </template>
 
 <script>
+import { queryWareHouseList } from '@/api/wareHouse'
+
 export default {
   props:['productInventoryWarningList'],
   data() {
     return {
+      wareHouseList: [],
       product_inventory_warning: {
         sel: null,
         columns: [
-          { field: 'wareHouseCode', title: '仓库编号', width: 120 },
+          { field: 'wareHouseCode', title: '仓库编号', width: 220, type: 'select' },
           { field: 'wareHouseName', title: '仓库名称', width: 120 },
           { field: 'minimumInventory', title: '最低库存', width: 160 },
           { field: 'highestInventory', title: '最高库存', width: 160 }
@@ -45,9 +55,11 @@ export default {
   created: function() {
     this.addProductInventoryWarning()
     this.product_inventory_warning.data = this.productInventoryWarningList
+    this.queryWareHouseList()
   },
   methods: {
     setProductInventoryWarning() {
+      console.log('点击仓库下拉框')
       this.$emit('setProductInventoryWarningList', this.product_inventory_warning.data)
     },
     addProductInventoryWarning() {
@@ -61,11 +73,17 @@ export default {
       this.product_inventory_warning.sel = JSON.parse(JSON.stringify(j))
     },
     delProductInventoryWarning(index) {
-      if ( this.product_inventory_warning.data.length > 1) {
+      if (this.product_inventory_warning.data.length > 1) {
         this.product_inventory_warning.data.splice(index, 1)
       } else {
-        this.$message.warning("至少保留一项")
+        this.$message.warning('至少保留一项')
       }
+    },
+    queryWareHouseList() {
+      queryWareHouseList().then(res => {
+        console.log('仓库列表' + res)
+        this.wareHouseList = res
+      })
     }
   }
 }
