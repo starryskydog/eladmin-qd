034eaf3912d13800925c98bc31e9075262b1e469
starrysky <838252223@qq.com>
2019-10-06 09:54:55 +0800

    耗材管理

---
+++ src/api/consumables.js
@@ -0,0 +1,43 @@
+import request from '@/utils/request'
+
+// 分页查询耗材
+export function queryConsumablesInfoPageList() {
+  return request({
+    url: 'api/queryConsumablesInfoPageList',
+    method: 'get'
+  })
+}
+
+// 查询耗材列表
+export function queryConsumablesInfoList() {
+  return request({
+    url: 'api/queryConsumablesInfoList',
+    method: 'get'
+  })
+}
+
+// 添加耗材
+export function add(data) {
+  return request({
+    url: 'api/consumablesInfo',
+    method: 'post',
+    data
+  })
+}
+
+// 修改耗材
+export function edit(data) {
+  return request({
+    url: 'api/consumablesInfo',
+    method: 'put',
+    data
+  })
+}
+
+// 删除耗材
+export function del(id) {
+  return request({
+    url: 'api/consumablesInfo/' + id,
+    method: 'delete'
+  })
+}
---
+++ src/views/bd/consumables/form.vue
@@ -0,0 +1,99 @@
+<template>
+  <el-dialog :visible.sync="dialog" :title="isAdd ? '新增耗材' : '编辑耗材'" append-to-body width="500px">
+    <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
+      <el-form-item label="计量单位名称" prop="name">
+        <el-input v-model="form.consumablesName" style="width: 370px;"/>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button type="text" @click="cancel">取消</el-button>
+      <el-button :loading="loading" type="primary" @click="doSubmit">确认</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import { add, edit } from '@/api/measureUnit'
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+export default {
+  props: {
+    isAdd: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      loading: false, dialog: false,
+      form: { consumablesName: '' },
+      rules: {
+        name: [
+          { required: true, message: '请输入名称', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    cancel() {
+      this.resetForm()
+    },
+    doSubmit() {
+      this.$refs['form'].validate((valid) => {
+        if (valid) {
+          this.loading = true
+          if (this.isAdd) {
+            this.doAdd()
+          } else this.doEdit()
+        } else {
+          return false
+        }
+      })
+    },
+    doAdd() {
+      add(this.form).then(res => {
+        this.resetForm()
+        this.$notify({
+          title: '添加成功',
+          type: 'success',
+          duration: 2500
+        })
+        this.loading = false
+        this.$parent.init()
+      }).catch(err => {
+        this.loading = false
+      })
+    },
+    doEdit() {
+      edit(this.form).then(res => {
+        this.resetForm()
+        this.$notify({
+          title: '修改成功',
+          type: 'success',
+          duration: 2500
+        })
+        this.loading = false
+        this.$parent.init()
+      }).catch(err => {
+        this.loading = false
+        console.log(err.response.data.message)
+      })
+    },
+    resetForm() {
+      this.dialog = false
+      this.$refs['form'].resetFields()
+      this.form = { name: ''}
+    },
+    changeScope() {
+      if (this.form.dataScope === '自定义') {
+        this.getDepts()
+      }
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  /deep/ .el-input-number .el-input__inner {
+    text-align: left;
+  }
+</style>
