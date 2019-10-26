e71f85f67d132c4ea75996ab0f9887bd41c052f0
starrysky <838252223@qq.com>
2019-10-26 12:40:44 +0800

    正式环境地址

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://api.auauz.net"'
+  // BASE_API: '"https://api.auauz.net"'
+  BASE_API: '"http://47.103.159.227:8088"'
 }
