3dc78906e08eb5ca5456e880bf5b311ead389caf
starrysky <838252223@qq.com>
2020-05-17 09:51:33 +0800

    产品统计

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"https://api.auauz.net"'
-  // BASE_API: '"http://47.103.159.227:8888"'
-  BASE_API: '"http://localhost:8888"'
+  BASE_API: '"http://47.103.159.227:8888"'
+  // BASE_API: '"http://localhost:8888"'
 }
